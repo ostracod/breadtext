1b47dae751926e6b5baaa54c70740cd19630bfef
John Eisenmann <esperantanaso@gmail.com>
2017-08-04 18:15:30 -0400

    Recording key sequence

--- src/fuzz.c
+++ src/fuzz.c
@@ -9,28 +9,48 @@
 #include "breadtext.h"
 #include "fuzz.h"
 
-#define FUZZ_KEY_AMOUNT 500
+#define FUZZ_KEY_AMOUNT 20
 
 fuzzKey_t fuzzKeySet[] = {
     {'t', NULL},
     {27, (int8_t *)"ESC"}
 };
 
+fuzzKey_t *fuzzKeyList[FUZZ_KEY_AMOUNT];
+int32_t fuzzKeyCount = 0;
+
 void performFuzzTest() {
     int32_t tempCount = 0;
     while (tempCount < FUZZ_KEY_AMOUNT) {
         int32_t index = rand() % (sizeof(fuzzKeySet) / sizeof(*fuzzKeySet));
         fuzzKey_t *tempFuzzKey = fuzzKeySet + index;
+        fuzzKeyList[fuzzKeyCount] = tempFuzzKey;
+        fuzzKeyCount += 1;
         handleKey(tempFuzzKey->key);
-        sleepMilliseconds(10);
+        sleepMilliseconds(1);
         tempCount += 1;
     }
+    // TEST CODE.
+    handleSegmentationFault(0);
 }
 
 void handleSegmentationFault(int signum) {
     // Set SEGFAULT behavior back to normal.
     signal(SIGSEGV, SIG_DFL);
     endwin();
-    printf("Failure!!!\n");
+    int8_t *tempPath = mallocRealpath((int8_t *)"./failure.txt");
+    FILE *tempFile = fopen((char *)tempPath, "w");
+    int32_t index = 0;
+    while (index < fuzzKeyCount) {
+        fuzzKey_t *tempFuzzKey = fuzzKeyList[index];
+        if (tempFuzzKey->name == NULL) {
+            fprintf(tempFile, "%c ", (char)tempFuzzKey->key);
+        } else {
+            fprintf(tempFile, "%s ", tempFuzzKey->name);
+        }
+        index += 1;
+    }
+    fprintf(tempFile, "\n");
+    fclose(tempFile);
     exit(1);
 }
