f225106c9f94da4d03d12258ab04681feede0cdc
John Eisenmann <esperantanaso@gmail.com>
2017-08-04 18:26:00 -0400

    Logging file contents

--- src/fuzz.c
+++ src/fuzz.c
@@ -7,6 +7,7 @@
 #include <signal.h>
 #include "utilities.h"
 #include "breadtext.h"
+#include "textLine.h"
 #include "fuzz.h"
 
 #define FUZZ_KEY_AMOUNT 20
@@ -18,14 +19,53 @@ fuzzKey_t fuzzKeySet[] = {
 
 fuzzKey_t *fuzzKeyList[FUZZ_KEY_AMOUNT];
 int32_t fuzzKeyCount = 0;
+int8_t *initialBufferContents;
+int8_t *lastBufferContents;
+
+int8_t *mallocBufferContents() {
+    int64_t tempLength = 0;
+    textLine_t *tempLine = rootTextLine;
+    tempLine = getLeftmostTextLine(tempLine);
+    while (true) {
+        tempLength += tempLine->textAllocation.length;
+        tempLine = getNextTextLine(tempLine);
+        if (tempLine == NULL) {
+            break;
+        } else {
+            tempLength += 1;
+        }
+    }
+    // For null character.
+    tempLength += 1;
+    int8_t *output = malloc(tempLength);
+    int64_t index = 0;
+    tempLine = rootTextLine;
+    tempLine = getLeftmostTextLine(tempLine);
+    while (true) {
+        int64_t tempLength = tempLine->textAllocation.length;
+        copyData(output + index, tempLine->textAllocation.text, tempLength);
+        index += tempLength;
+        tempLine = getNextTextLine(tempLine);
+        if (tempLine == NULL) {
+            output[index] = 0;
+            break;
+        } else {
+            output[index] = '\n';
+            index += 1;
+        }
+    }
+    return output;
+}
 
 void performFuzzTest() {
+    initialBufferContents = mallocBufferContents();
     int32_t tempCount = 0;
     while (tempCount < FUZZ_KEY_AMOUNT) {
         int32_t index = rand() % (sizeof(fuzzKeySet) / sizeof(*fuzzKeySet));
         fuzzKey_t *tempFuzzKey = fuzzKeySet + index;
         fuzzKeyList[fuzzKeyCount] = tempFuzzKey;
         fuzzKeyCount += 1;
+        lastBufferContents = mallocBufferContents();
         handleKey(tempFuzzKey->key);
         sleepMilliseconds(1);
         tempCount += 1;
@@ -40,6 +80,7 @@ void handleSegmentationFault(int signum) {
     endwin();
     int8_t *tempPath = mallocRealpath((int8_t *)"./failure.txt");
     FILE *tempFile = fopen((char *)tempPath, "w");
+    fprintf(tempFile, "%s\n\n", initialBufferContents);
     int32_t index = 0;
     while (index < fuzzKeyCount) {
         fuzzKey_t *tempFuzzKey = fuzzKeyList[index];
@@ -50,7 +91,7 @@ void handleSegmentationFault(int signum) {
         }
         index += 1;
     }
-    fprintf(tempFile, "\n");
+    fprintf(tempFile, "\n\n%s\n", lastBufferContents);
     fclose(tempFile);
     exit(1);
 }
