d7eeda5ca390812d32a844f95465669a7fb4cc07
John Eisenmann <esperantanaso@gmail.com>
2017-09-02 11:43:21 -0400

    Added script to run all systematic tests

---
+++ systematicTest.sh
@@ -0,0 +1,32 @@
+#/usr/bin/sh
+
+TEST_COUNT=0
+FAILURE_COUNT=0
+
+if [ ! -d "./systematicTestResults" ]; then
+	mkdir ./systematicTestResults
+fi
+
+echo " "
+
+for FILE in ./systematicTests/*; do
+	DESTINATION="./systematicTestResults/testResult${TEST_COUNT}.txt"
+	echo "Running test:"
+	echo $FILE
+	echo $DESTINATION
+	breadtext --test $FILE
+	if [ $? -eq 0 ]; then
+		echo "PASSED"
+	else
+		echo "FAILED"
+		((FAILURE_COUNT++))
+	fi
+	echo " "
+	mv ./systematicTestResult.txt $DESTINATION
+	((TEST_COUNT++))
+done
+
+echo "================================="
+echo "FAILED TESTS: ${FAILURE_COUNT} / ${TEST_COUNT}"
+echo " "
+
